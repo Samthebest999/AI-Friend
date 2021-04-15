843f604f43fa2317dc002b3f4c775fcfd95f2f56
Samthebest999 <samit.mohnot@gmail.com>
2021-04-14 20:20:07 -0500

    update stuff

--- browser.py
+++ browser.py
@@ -6,18 +6,7 @@ from imports import sr, time
 browser = str(open("browser.txt", "rt").read())
 
 
-def recognize_speech():
-    r = sr.Recognizer()
-    mic = sr.Microphone()
-    with mic as source:
-        audio = r.listen(source)
-        r.adjust_for_ambient_noise(source)
-        global users_text
-        users_text = r.recognize_google(audio)
-
-
-say_stuff("Hi, Which website would you like to go to?", False)
-recognize_speech()
+users_text = input(say_stuff("Hi, Which website would you like to go to?", False))
 website = " https://" + users_text
 pyautogui.hotkey("winleft", "r")
 time.sleep(0.50)
