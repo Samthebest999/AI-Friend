9bc08873aa776d4d6d3aebf7f10afb0c6caff2ca
Samthebest999 <samit.mohnot@gmail.com>
2021-04-14 19:22:12 -0500

    Update updater AGAIN!
    Also change version.toml to latest version

--- updater.py
+++ updater.py
@@ -2,11 +2,11 @@ import os
 import wget
 
 os.remove("browser.py")
-wget.download("https://raw.githubusercontent.com/Samthebest999/AI-Friend/main/CODE/browser.py")
+wget.download("https://raw.githubusercontent.com/Samthebest999/AI-Friend/main/browser.py")
 os.remove("functions.py")
-wget.download("https://raw.githubusercontent.com/Samthebest999/AI-Friend/main/CODE/functions.py")
+wget.download("https://raw.githubusercontent.com/Samthebest999/AI-Friend/main/functions.py")
 os.remove("imports.py")
-wget.download("https://raw.githubusercontent.com/Samthebest999/AI-Friend/main/CODE/imports.py")
+wget.download("https://raw.githubusercontent.com/Samthebest999/AI-Friend/main/imports.py")
 os.remove("main.py")
-wget.download("https://raw.githubusercontent.com/Samthebest999/AI-Friend/main/CODE/main.py")
+wget.download("https://raw.githubusercontent.com/Samthebest999/AI-Friend/main/main.py")
 os.system("python main.py")
